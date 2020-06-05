02a3cb228abc2b9b2995a189f895cad0af2521bf
mprudencioluna <mprudencioluna@gmail.com>
2020-06-04 20:39:42 -0500

    New WEB Project

---
+++ TaquillerasWeb/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TaquillerasWeb.Data;
+
+[assembly: HostingStartup(typeof(TaquillerasWeb.Areas.Identity.IdentityHostingStartup))]
+namespace TaquillerasWeb.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<TaquillerasWebContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("TaquillerasWebContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                    .AddEntityFrameworkStores<TaquillerasWebContext>();
+            });
+        }
+    }
+}
---
+++ TaquillerasWeb/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TaquillerasWeb.Models;
+
+namespace TaquillerasWeb.Controllers
+{
+
+
+
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public HomeController(UserManager<IdentityUser> userManager,
+            ILogger<HomeController> logger)
+        {
+            _userManager = userManager;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            var user = _userManager.GetUserAsync(User).Result;
+            if (!user.EmailConfirmed)
+            {
+                return LocalRedirect(Url.Content("~/Identity/Account/Manage/ChangePassword"));
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
